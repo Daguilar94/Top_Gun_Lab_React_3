6d4c62aebed1e41258670d24fce0e08dfa57c569
David Aguilar <daguilar@arrivelogistics.com>
2019-06-14 17:53:44 -0500

    Create instructions for api calls

--- src/containers/CharacterList.js
+++ src/containers/CharacterList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Character from '../components/Character';
 
 class CharacterList extends Component {
     constructor(props) {
@@ -22,58 +21,9 @@ class CharacterList extends Component {
          }
     }
 
-    componentDidMount = () => {
-        this.getCharacters();
-    }
+    getCharacters = () => {}
 
-    getCharacters = () => {
-        axios.get(`${BASE_LOCAL_ENDPOINT}/characters`)
-        .then(response => {
-            this.setState({
-                characters: {
-                    content: response.data,
-                    error: ''
-                },
-                createCharacterError: false
-            })
-        })
-        .catch(error => {
-            this.setState({
-                characters: {
-                    error: error.message
-                }
-            })
-        })
-    }
-
-    createCharacter = (e) => {
-        e.preventDefault();
-        const {
-            newCharacterFrom: {
-                name,
-                location,
-                status,
-                species,
-                gender,
-                origin,
-                image
-            }
-        } = this.state;
-
-        axios.post(`${BASE_LOCAL_ENDPOINT}/characters`, {
-            name,
-            location,
-            status,
-            species,
-            gender,
-            origin,
-            image
-        }, {
-            headers: { "Content-Type": "application/json"}
-        })
-        .then(() => { this.getCharacters() })
-        .catch(() => { this.setState({ createCharacterError: true })})
-    }
+    createCharacter = (e) => {}
 
     createTextInput = (value, field) => (
         <input
@@ -96,8 +46,6 @@ class CharacterList extends Component {
 
     render() { 
         const {
-            createCharacterError,
-            characters: { content, error },
             newCharacterFrom: {
                 name,
                 location,
@@ -109,15 +57,10 @@ class CharacterList extends Component {
             }
         } = this.state;
 
-        if (error) {
-            return <div>Fetch Error: {error}</div>
-        }
-
         return (
             <>  
                 <h2>Create Character</h2>
 
-                {createCharacterError && <p>An error ocurred creating Character</p>}
                 <form onSubmit={e => this.createCharacter(e)}>
                     {this.createTextInput(name, 'name')}
                     {this.createTextInput(location, 'location')}
@@ -128,10 +71,6 @@ class CharacterList extends Component {
                     {this.createTextInput(image, 'image')}
                     <button type="submit">Create</button>
                 </form>
-
-                {content.map(({ id, image, name }) => (
-                    <Character key={id} imgSrc={image} name={name}/>
-                ))}
             </>
         );
     }
@@ -144,9 +83,33 @@ export default CharacterList;
 
 //2. Importar el endpoint de constants.js
 
-//3. En la function "getCharacters" realiza un llamado al endpoint "http://localhost:3004/characters",
+//3. En la function "getCharacters" realiza un llamado GET al endpoint "http://localhost:3004/characters",
 // (Utiliza la constante que importaste en el paso 2).
 // este endpoint devuelve un arreglo de personajes de la serie Rick and Morty, debes guardar
 // este arreglo en el estado, específicamente en la llave content del objeto characters.
 
-//4. 
+//4. Una vez se monte el componente, llama la función "getCharacters" para obtener la información.
+
+//5. Después del formulario, Itera sobre el arreglo de personajes que ya está guardado en el estado y por cada uno
+// renderiza su imagen y nombre utilizando el componente "Character" ubicado en "/components/Character.js"
+
+//6. Si en la petición realizada en el punto 3 ocurre un error, el componente sólo debe mostrar el
+// mensaje de error devuelto en la petición.
+
+//7. Cuando Se llene el formulario de creación y se le haga submit (En la función "createCharacter"), Realizar una petición POST al
+// endpoint "http://localhost:3004/characters" con la información ingresada.
+// el endpoint recibe los siguientes campos:
+
+// name
+// location
+// status
+// species
+// gender
+// origin
+// image
+
+// 8. Si la creación del personaje se realiza con éxito, vuelve a llamar la función "getCharacters"
+// para que se vuelvan a cargar los personajes ya actualizados.
+
+//9. En caso de que ocurra un error en la creación del usuario, mostrar un mensaje de error
+// encima del formulario.
