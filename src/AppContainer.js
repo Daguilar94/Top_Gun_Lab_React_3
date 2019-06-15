083dd651acce25f28a4e6d22d0bbc5cc4464fa4a
David Aguilar <davidaguilarh.94@gmail.com>
2019-06-14 22:15:35 -0500

    Start writing routing instructions

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import Home from './components/Home';
-import NotFound from './components/NotFound'
+import NotFound from './components/NotFound';
 import CharacterDetails from './containers/CharacterDetails';
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 function AppContainer() {
   return (
-    <HashRouter>
-      <Switch>
-        <Route
-            exact
-            path="/"
-            render={() => (
-                <Redirect to="/characters" />
-            )}
-        />
-        <Route exact path="/characters" component={Home} />
-        <Route exact path="/characters/:id" component={CharacterDetails} />
-        <Route component={NotFound} />
-      </Switch>
-    </HashRouter>
   );
 }
 
 export default AppContainer;
+
+//-----------------------------------------------------------------------------------------------
+//1. Importar el componente Home de "components/Home.js"
+
+//2. Importar los siguientes componentes de "react-router-dom":
+// HashRouter
+// Route
+// Switch
+// Redirect
+
+//3. crear un El routing principal para la aplicación (con BrowserRouter o HashRouter)
